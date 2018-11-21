90cede093f9a7b57f05db990a316bffd2296b4a3
Alvin Tanurwijaya <alvin8876@gmail.com>
2018-11-21 23:28:51 +0700

    fix date and returned theme

--- src/helper/Theme.js
+++ src/helper/Theme.js
@@ -2,7 +2,7 @@ import Dark from '../theme/Dark'
 import Light from '../theme/Light'
 
 export function getTheme() {
-  if (new Date().getHours() < 20) {
+  if (new Date().getHours() > 20) {
     return Dark
   } else {
     return Light

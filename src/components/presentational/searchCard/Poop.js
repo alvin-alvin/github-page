0a574813ce8ee68d3057e186db6cf3a6babe2ee8
Alvin Tanurwijaya <alvin8876@gmail.com>
2018-12-31 15:28:57 +0700

    Updates

--- src/components/presentational/searchCard/Poop.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export default function AgeCard() {
-  return (
-    <div style={{display:'flex'}}>
-    <p style={{margin:0,fontSize:42,alignSelf:'center',textAlign:'center'}}>💩</p>
-    </div>
-  )
-}
--- src/helper/ResponseList.js
+++
@@ -1,8 +0,0 @@
-export const RESPONSE_LIST = {
-
-  UMUR: '21',
-  KULIAH: 'Saat ini sedang berkuliah di Binus dengan jurusan mobile application and technology',
-  LAHIR: 'Lahir di Palembang, 10 September 1997',
-  EXPERIENCE : '',
-  POO: '💩',
-}
